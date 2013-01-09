316063ea113384808f5879077d16d23545d3ba00
Udo Stenzel <udo.stenzel@eva.mpg.de>
2013-01-09 15:22:05 +0100

    view understand EF flag now
    We formerly used XF for 'extended flags' and promptly collided with
    BWA's use of it.  We use EF now, and samtools respects it.  If EF is
    missing, it will look at XF instead.

--- sam_view.c
+++ sam_view.c
@@ -38,7 +38,8 @@ static inline int __g_skip_aln(const bam_header_t *h, const bam1_t *b)
 	int l = (b->core.flag & BAM_FPROPER_PAIR) && b->core.isize ? abs(b->core.isize) : b->core.l_qseq ;
     int flags = b->core.flag ;
     if( (g_flag_on | g_flag_off) & ~0xffff ) {
-        char *s = bam_aux_get(b, "XF");
+        uint8_t *s = bam_aux_get(b, "EF");
+        if( !s ) s = bam_aux_get(b, "XF");
         flags |= bam_aux2i(s) << 16 ;
     }
 
